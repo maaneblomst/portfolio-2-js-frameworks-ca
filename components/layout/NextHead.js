94a82eda6ae1e5b8b8dae68c61ef326b1b89042d
maaneblomst <maaneblomst@users.noreply.github.com>
2021-10-02 18:01:04 +0200

    Create NextHead.js

---
+++ components/layout/NextHead.js
@@ -0,0 +1,20 @@
+import NextHead from "next/head";
+
+export default function Head({ title = "" }) {
+  return (
+    <NextHead>
+      <title>
+        {title}
+        {title ? " | " : ""}JS Frameworks CA
+      </title>
+      <meta property="og:title" content={title} key={title} />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
+        crossorigin="anonymous"
+      ></link>
+    </NextHead>
+  );
+}
