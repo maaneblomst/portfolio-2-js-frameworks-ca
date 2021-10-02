a62849841158f845a52aa930db7575b7295b7d7a
maaneblomst <maaneblomst@users.noreply.github.com>
2021-10-02 18:01:27 +0200

    Update next.config.js

--- config/next.config.js
+++ config/next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["https://www.holmcreations.com/test-api/"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "/admin",
+        permanent: false,
+      },
+    ];
+  },
 };
